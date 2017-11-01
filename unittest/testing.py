53d6fc477fd4eed003a43e4d358f00754964c071
Markus Koskimies <markus.koskimies@softability.fi>
2017-11-01 17:17:09 +0200

    Testing escape velocities.

--- unittest/testing.py
+++ unittest/testing.py
@@ -6,7 +6,34 @@
 #
 ###############################################################################
 
+import os, sys
+sys.path.append(os.path.abspath("."))
+
 from orbtools import *
+from sol import *
+
+#------------------------------------------------------------------------------
+# Testing escape velocities (velocities at infinite)
+#------------------------------------------------------------------------------
+
+import math
+
+def Earth_escape(altitude, v_inf):
+	r     = Earth.radius + altitude
+	v_esc = Earth.v_escape(r)
+	dv    = solve_rvrv(Earth.GM, r, None, float("inf"), v_inf)
+	print "v_esc=%.2f v_inf=%7.2f dv=%.2f (d=%7.2f)" % (v_esc, v_inf, dv, dv - v_esc)
+	return dv
+
+Earth_escape(300e3, 0e3)
+Earth_escape(300e3, 1e3)
+Earth_escape(300e3, 3e3)
+Earth_escape(300e3, 5e3)
+
+sys.exit(0)
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
 # print solve_rocket_eq(None, 1, 9000, 4500)
 # print solve_rocket_eq(1, None, 9000, 4500)
