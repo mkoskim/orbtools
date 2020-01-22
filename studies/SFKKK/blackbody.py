addb4ffbec1299052410adfa0c366f116c6fb778
Markus Koskimies <mkoskim@gmail.com>
2020-01-22 21:36:45 +0200

    Finetuning

--- studies/SFKKK/blackbody.py
+++ studies/SFKKK/blackbody.py
@@ -12,18 +12,20 @@ from orbtools.systems.exoplanets import *
 
 spectrum = range(50, 2000, 50)
 
-def makebb(T):
+def makebb(T, W):
     bb = []
     for wl in spectrum:
         bb.append(blackbody(T, wl * 1e-9))
 
     E = sum(bb)
-    return list(map(lambda x: x/E, bb))
+    return list(map(lambda x: W * x/E, bb))
 
-G2V = makebb(5780)
-K0  = makebb(5240)
+G2 = makebb(5780, 1300)
+K0 = makebb(5240, 1300)
+F5 = makebb(6540, 1300)
 
-#for p in G2V: print(p)
-for p in K0: print(p)
+#for p in G2: print(p)
+#for p in K0: print(p)
+for p in F5: print(p)
 #for wl in spectrum: print wl
 
