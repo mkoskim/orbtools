9faa9d9b5b3c2afff2e9239cd1e8b99c8eaf78c0
Markus Koskimies <mkoskim@gmail.com>
2022-11-12 16:19:46 +0200

    Studying LTP concept

---
+++ studies/Engines/LaserThermalEngine.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+###############################################################################
+#
+# Fictive engine SSE-1 for ascending from planet surfaces
+#
+# SSE, Speculative Space Engines
+#
+###############################################################################
+
+import os, sys
+sys.path.append(os.path.abspath("."))
+
+from orbtools.systems.solsystem import *
+from orbtools.systems.exoplanets import *
+
+#------------------------------------------------------------------------------
+# See: https://arxiv.org/pdf/2201.00244.pdf
+#------------------------------------------------------------------------------
+
+LTE = Engine(Isp2ve(3000), P = 100e6)
+
+LTE.info()
