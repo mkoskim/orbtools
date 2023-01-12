f272eff6cf49dca6ba07184f39ec13b49c86af1b
Markus Koskimies <mkoskim@gmail.com>
2023-01-12 18:23:04 +0200

    Separated database utils

--- orbtools/systems/exoplanets/__init__.py
+++ orbtools/systems/exoplanets/__init__.py
@@ -3,3 +3,12 @@ sys.path.append(os.path.abspath("."))
 
 from orbtools.systems.exoplanets.oec import *
 #from orbtools.systems.exoplanets_eu import *
+
+from orbtools.systems.exoplanets.utils import *
+
+print("Exoplanets imported")
+
+
+
+#print("Stars...:", len(stars.keys()))
+#print("Planets.:", len(planets.keys()))
